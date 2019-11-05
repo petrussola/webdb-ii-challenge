455224e18846208c5a6719186324bcbd57b27e2e
Pere <pere.solaclaver@gmail.com>
2019-11-05 14:58:35 +0000

    Get all cars method end point retrieving all cars in DB

--- data/helpers/carsRouter.js
+++ data/helpers/carsRouter.js
@@ -4,7 +4,15 @@ const Cars = require("./carsDb");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.status(200).json({ message: `welcome from dummy endpoint!` });
+  Cars.get()
+    .then(data => {
+      res.status(200).json(data);
+    })
+    .catch(error => {
+      res
+        .status(500)
+        .json({ message: `Data could not be retrieved: ${error.message}` });
+    });
 });
 
 module.exports = router;
