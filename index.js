92f25ef99fee67b54c7344d36c02981697358d7b
Pere <pere.solaclaver@gmail.com>
2019-11-05 13:28:09 +0000

    Scaffold express app and server listening on chosen port

---
+++ index.js
@@ -0,0 +1,7 @@
+const server = require("./server");
+
+const port = process.env.PORT;
+
+server.listen(port, () => {
+  console.log(`\n***Server Running on port ${port}***\n`);
+});
--- server.js
+++ server.js
@@ -1,2 +1,20 @@
-const express = require('express');
-const cors = require('cors');
+// IMPORT DEPENDENCIES
+
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+require('dotenv').config();
+
+// INITIALIZE EXPRESS INSTANCE
+
+const server = express();
+
+// MIDDLEWARE
+
+server.use(cors());
+server.use(helmet());
+server.use(express.json());
+
+// EXPORTS
+
+module.exports = server;
